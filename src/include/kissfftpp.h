dd37454d3757df03aaccf44433b075e96ad11628
Kevin Venalainen <kjvenalainen@gmail.com>
2024-06-08 23:03:16 -0700

    working for all butterflies

--- src/include/kissfftpp.h
+++ src/include/kissfftpp.h
@@ -41,44 +41,168 @@
 #if defined(KFFTPP_NO_CONTRACT_CHECKING)
 #define KFFTPP_ASSERT(cond, msg) ((void)0)
 #elif defined(KFFTPP_NO_EXCEPTIONS)
-#define KFFTPP_ASSERT(cond, msg)                                               \
-  if (!(cond)) {                                                               \
-    std::terminate();                                                          \
+#define KFFTPP_ASSERT(cond, msg) \
+  if (!(cond)) {                 \
+    std::terminate();            \
   }
 #else
-#define KFFTPP_ASSERT(cond, msg)                                               \
-  if (!(cond)) {                                                               \
-    throw std::logic_error(msg);                                               \
+#define KFFTPP_ASSERT(cond, msg) \
+  if (!(cond)) {                 \
+    throw std::logic_error(msg); \
   }
 #endif
 
 #ifndef KFFTPP_HALF_PI
 #define KFFTPP_HALF_PI 1.5707963267948966192313216916397514420986L
-#endif // KFFTPP_HALF_PI
+#endif  // KFFTPP_HALF_PI
 
 #ifndef KFFTPP_PI
 #define KFFTPP_PI 3.1415926535897932384626433832795028841972L
-#endif // KFFTPP_PI
+#endif  // KFFTPP_PI
 
 namespace kfft {
 
 namespace internal {
 
+// Use custom complex number implementation to avoid slow std::complex
+// operations due to NaN propagation rules.
+template <typename T>
+class complex {
+ public:
+  constexpr complex(const T& real = T(), const T& imag = T())
+      : real_(real), imag_(imag) {}
+  constexpr complex(const complex&) = default;
+  constexpr complex(complex&&) = default;
+
+  constexpr T real() const { return real_; }
+  constexpr void real(T value) { real_ = value; }
+  constexpr T imag() const { return imag_; }
+  constexpr void imag(T value) { imag_ = value; }
+
+  constexpr complex& operator=(const complex&) = default;
+  constexpr complex& operator+=(const complex& rhs) {
+    real_ += rhs.real_;
+    imag_ += rhs.imag_;
+    return *this;
+  }
+  constexpr complex& operator-=(const complex& rhs) {
+    real_ -= rhs.real_;
+    imag_ -= rhs.imag_;
+    return *this;
+  }
+  constexpr complex& operator*=(const complex& rhs) {
+    const T real = real_ * rhs.real_ - imag_ * rhs.imag_;
+    const T imag = real_ * rhs.imag_ + imag_ * rhs.real_;
+    real_ = real;
+    imag_ = imag;
+    return *this;
+  }
+  constexpr complex& operator/=(const complex& rhs) {
+    const T div = rhs.real_ * rhs.real_ + rhs.imag_ * rhs.imag_;
+    const T real = (real_ * rhs.real_ + imag_ * rhs.imag_) / div;
+    const T imag = (imag_ * rhs.real_ - real_ * rhs.imag_) / div;
+    real_ = real;
+    imag_ = imag;
+    return *this;
+  }
+
+  constexpr complex& operator+=(const T& rhs) noexcept {
+    real_ += rhs;
+    return *this;
+  }
+  constexpr complex& operator-=(const T& rhs) noexcept {
+    real_ -= rhs;
+    return *this;
+  }
+  constexpr complex& operator*=(const T& rhs) noexcept {
+    real_ *= rhs;
+    imag_ *= rhs;
+    return *this;
+  }
+  constexpr complex& operator/=(const T& rhs) noexcept {
+    real_ /= rhs;
+    imag_ /= rhs;
+    return *this;
+  }
+
+ private:
+  T real_;
+  T imag_;
+};
+
+template <typename T>
+constexpr complex<T> operator+(const complex<T>& val) {
+  return {val.real(), val.imag()};
+}
+template <typename T>
+constexpr complex<T> operator-(const complex<T>& val) {
+  return {-val.real(), -val.imag()};
+}
+template <typename T>
+constexpr complex<T> operator+(const complex<T>& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) += rhs;
+}
+template <typename T>
+constexpr complex<T> operator+(const T& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) += rhs;
+}
+template <typename T>
+constexpr complex<T> operator+(const complex<T>& lhs, const T& rhs) {
+  return complex<T>(lhs) += rhs;
+}
+template <typename T>
+constexpr complex<T> operator-(const complex<T>& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) -= rhs;
+}
+template <typename T>
+constexpr complex<T> operator-(const T& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) -= rhs;
+}
+template <typename T>
+constexpr complex<T> operator-(const complex<T>& lhs, const T& rhs) {
+  return complex<T>(lhs) -= rhs;
+}
+template <typename T>
+constexpr complex<T> operator*(const complex<T>& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) *= rhs;
+}
+template <typename T>
+constexpr complex<T> operator*(const T& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) *= rhs;
+}
+template <typename T>
+constexpr complex<T> operator*(const complex<T>& lhs, const T& rhs) {
+  return complex<T>(lhs) *= rhs;
+}
+template <typename T>
+constexpr complex<T> operator/(const complex<T>& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) /= rhs;
+}
+template <typename T>
+constexpr complex<T> operator/(const T& lhs, const complex<T>& rhs) {
+  return complex<T>(lhs) /= rhs;
+}
+template <typename T>
+constexpr complex<T> operator/(const complex<T>& lhs, const T& rhs) {
+  return complex<T>(lhs) /= rhs;
+}
+
 // Lightweight span implementation.
-template <typename T> class span {
-public:
+template <typename T>
+class span {
+ public:
   // Placeholder for getting all elements during subspan.
   static constexpr size_t ALL = SIZE_MAX;
 
   // Constructors.
-  span(T *ptr, size_t size) noexcept : ptr_(ptr), size_(size) {}
-  span(const span &) = default;
-  span(span &&) = default;
+  span(T* ptr, size_t size) noexcept : ptr_(ptr), size_(size) {}
+  span(const span&) = default;
+  span(span&&) = default;
 
   // Accessors.
-  T *data() const noexcept { return ptr_; }
+  T* data() const noexcept { return ptr_; }
   size_t size() const noexcept { return size_; }
-  T &operator[](size_t i) const {
+  T& operator[](size_t i) const {
     KFFTPP_ASSERT(i < size_, "Index out of bounds");
     return ptr_[i];
   }
@@ -91,16 +215,16 @@ public:
     return span(ptr_ + offset, count == ALL ? size_ - offset : count);
   }
 
-private:
-  T *ptr_;
+ private:
+  T* ptr_;
   size_t size_;
 };
 
 // Compute twiddle factors for FFT of length N.
 template <typename T>
-static constexpr std::vector<std::complex<T>> ComputeTwiddles(size_t N,
-                                                              bool inverse) {
-  auto twiddles = std::vector<std::complex<T>>(N);
+static constexpr std::vector<internal::complex<T>> ComputeTwiddles(
+    size_t N, bool inverse) {
+  auto twiddles = std::vector<internal::complex<T>>(N);
   const double phase = inverse ? 2 * KFFTPP_PI / N : -2 * KFFTPP_PI / N;
   for (size_t i = 0; i < N; ++i) {
     const double phaseArg = phase * i;
@@ -110,8 +234,8 @@ static constexpr std::vector<std::complex<T>> ComputeTwiddles(size_t N,
   return twiddles;
 }
 
-// Factorize N, first as powers of 4, then powers of 2, then remaining
-// primes.
+// Given a FFT length N, factorize it into a sequence of `p, m` pairs where `p`
+// is the FFT radix and `m` is the length of the FFT at that stage.
 static std::vector<int> Factorize(size_t N) {
   auto factors = std::vector<int>();
   int p = 4;
@@ -119,18 +243,17 @@ static std::vector<int> Factorize(size_t N) {
   do {
     while (N % p) {
       switch (p) {
-      case 4:
-        p = 2;
-        break;
-      case 2:
-        p = 3;
-        break;
-      default:
-        p += 2;
-        break;
+        case 4:
+          p = 2;
+          break;
+        case 2:
+          p = 3;
+          break;
+        default:
+          p += 2;
+          break;
       }
-      if (p > floorSqrt)
-        p = N; // no more factors, skip to end
+      if (p > floorSqrt) p = N;  // no more factors, skip to end
     }
     N /= p;
     factors.push_back(p);
@@ -139,10 +262,37 @@ static std::vector<int> Factorize(size_t N) {
   return factors;
 }
 
+// Given a factorized FFT, compute the maximum required scratch array length for
+// the generic butterfly operations. If there are no generic butterflies, then
+// the scratch space required is 0.
+static constexpr size_t RequiredScratchLength(const std::vector<int>& factors) {
+  constexpr std::array<int, 4> NON_GENERIC_BUTTERFLY_RADICES = {2, 3, 4, 5};
+
+  size_t scratchLength = 0;
+  for (size_t i = 0; i < factors.size(); i += 2) {
+    bool generic = true;
+    for (size_t pi = 0; pi < NON_GENERIC_BUTTERFLY_RADICES.size(); ++pi) {
+      if (factors[i] == NON_GENERIC_BUTTERFLY_RADICES[pi]) {
+        // Not a generic butterfly, skip.
+        generic = false;
+        break;
+      }
+    }
+    if (!generic) {
+      continue;
+    }
+
+    // Generic butterfly, compute scratch size.
+    scratchLength = std::max(scratchLength, static_cast<size_t>(factors[i]));
+  }
+
+  return scratchLength;
+}
+
 template <typename T>
-static constexpr void
-Butterfly2(internal::span<T> x, const size_t stride,
-           const std::vector<std::complex<float>> &twiddles, const size_t m) {
+static constexpr void Butterfly2(
+    internal::span<T> x, const size_t stride,
+    const std::vector<internal::complex<float>>& twiddles, const size_t m) {
   for (size_t i = 0; i < m; ++i) {
     const auto xi = x[m + i] * twiddles[i * stride];
     x[m + i] = x[i] - xi;
@@ -151,51 +301,140 @@ Butterfly2(internal::span<T> x, const size_t stride,
 }
 
 template <typename T>
-static constexpr void
-Butterfly4(internal::span<T> x, const size_t stride,
-           const std::vector<std::complex<float>> &twiddles, const size_t m) {
-  const int m2 = 2 * m;
-  const int m3 = 3 * m;
-  const size_t stride2 = 2 * stride;
-  const size_t stride3 = 3 * stride;
-  std::array<T, 6> scratch;
+static constexpr void Butterfly3(
+    internal::span<T> x, const size_t stride,
+    const std::vector<internal::complex<float>>& twiddles, const size_t m) {
+  const size_t m2 = 2 * m;
   for (size_t i = 0; i < m; ++i) {
-    scratch[0] = x[m + i] * twiddles[i * stride];
-    scratch[1] = x[m2 + i] * twiddles[i * stride2];
-    scratch[2] = x[m3 + i] * twiddles[i * stride3];
-    scratch[5] = x[i] - scratch[1];
-    x[i] += scratch[1];
-    scratch[3] = scratch[0] + scratch[2];
-    scratch[4] = scratch[0] - scratch[2];
-    x[m2 + i] = x[i] - scratch[3];
-    x[i] += scratch[3];
-
-    // TODO: Different for inverse.
-    x[m + i] = {scratch[5].real() + scratch[4].imag(),
-                scratch[5].imag() - scratch[4].real()};
-    x[m3 + i] = {scratch[5].real() - scratch[4].imag(),
-                 scratch[5].imag() + scratch[4].real()};
+    std::array<T, 5> xi;
+    xi[1] = x[m + i] * twiddles[i * stride];
+    xi[2] = x[m2 + i] * twiddles[i * 2 * stride];
+    xi[3] = xi[1] + xi[2];
+    xi[0] = xi[1] - xi[2];
+    x[m + i] = {
+        x[i].real() - (xi[3].real() * 0.5f),
+        x[i].imag() - (xi[3].imag() * 0.5f),
+    };
+    xi[0] *= twiddles[m * stride].imag();
+    x[i] += xi[3];
+    x[m2 + i] = {x[m + i].real() + xi[0].imag(),
+                 x[m + i].imag() - xi[0].real()};
+    x[m + i] = {x[m + i].real() - xi[0].imag(), x[m + i].imag() + xi[0].real()};
   }
 }
 
 template <typename T>
-static constexpr void
-FftRecursive(const internal::span<const T> x, internal::span<T> y,
-             const size_t inputStride, const size_t factorStride,
-             const size_t recursionIndex, const std::vector<int> &factors,
-             const std::vector<std::complex<float>> &twiddles) {
-  // // recombine the p smaller DFTs
-  // switch (p) {
-  //     case 2: kf_bfly2(Fout,fstride,st,m); break;
-  //     case 3: kf_bfly3(Fout,fstride,st,m); break;
-  //     case 4: kf_bfly4(Fout,fstride,st,m); break;
-  //     case 5: kf_bfly5(Fout,fstride,st,m); break;
-  //     default: kf_bfly_generic(Fout,fstride,st,m,p); break;
-  // }
+static constexpr void Butterfly4(
+    internal::span<T> x, const size_t stride,
+    const std::vector<internal::complex<float>>& twiddles, const size_t m) {
+  const size_t m2 = 2 * m;
+  const size_t m3 = 3 * m;
+  for (size_t i = 0; i < m; ++i) {
+    std::array<T, 6> xi;
+    xi[0] = x[m + i] * twiddles[i * stride];
+    xi[1] = x[m2 + i] * twiddles[i * 2 * stride];
+    xi[2] = x[m3 + i] * twiddles[i * 3 * stride];
+    xi[3] = xi[0] + xi[2];
+    xi[4] = xi[0] - xi[2];
+    xi[5] = x[i] - xi[1];
+    x[i] += xi[1];
+    x[m2 + i] = x[i] - xi[3];
+    x[i] += xi[3];
 
-  const int p = factors[2 * recursionIndex]; // FFT radix for this stage.
+    // TODO: Different for inverse.
+    x[m + i] = {xi[5].real() + xi[4].imag(), xi[5].imag() - xi[4].real()};
+    x[m3 + i] = {xi[5].real() - xi[4].imag(), xi[5].imag() + xi[4].real()};
+  }
+}
+
+template <typename T>
+static constexpr void Butterfly5(
+    internal::span<T> x, const size_t stride,
+    const std::vector<internal::complex<float>>& twiddles, const size_t m) {
+  const size_t m2 = 2 * m;
+  const size_t m3 = 3 * m;
+  const size_t m4 = 4 * m;
+  for (size_t i = 0; i < m; ++i) {
+    std::array<T, 13> xi;
+    xi[0] = x[i];
+    xi[1] = x[m + i] * twiddles[i * stride];
+    xi[2] = x[m2 + i] * twiddles[i * 2 * stride];
+    xi[3] = x[m3 + i] * twiddles[i * 3 * stride];
+    xi[4] = x[m4 + i] * twiddles[i * 4 * stride];
+    xi[7] = xi[1] + xi[4];
+    xi[10] = xi[1] - xi[4];
+    xi[8] = xi[2] + xi[3];
+    xi[9] = xi[2] - xi[3];
+    x[i] = {
+        xi[0].real() + xi[7].real() + xi[8].real(),
+        xi[0].imag() + xi[7].imag() + xi[8].imag(),
+    };
+    xi[5] = {
+        xi[0].real() + xi[7].real() * twiddles[m * stride].real() +
+            xi[8].real() * twiddles[m * 2 * stride].real(),
+        xi[0].imag() + xi[7].imag() * twiddles[m * stride].real() +
+            xi[8].imag() * twiddles[m * 2 * stride].real(),
+    };
+    xi[6] = {
+        xi[10].imag() * twiddles[m * stride].imag() +
+            xi[9].imag() * twiddles[m * 2 * stride].imag(),
+        -xi[10].real() * twiddles[m * stride].imag() -
+            xi[9].real() * twiddles[m * 2 * stride].imag(),
+    };
+    x[m + i] = xi[5] - xi[6];
+    x[m4 + i] = xi[5] + xi[6];
+    xi[11] = {
+        xi[0].real() + xi[7].real() * twiddles[m * 2 * stride].real() +
+            xi[8].real() * twiddles[m * stride].real(),
+        xi[0].imag() + xi[7].imag() * twiddles[m * 2 * stride].real() +
+            xi[8].imag() * twiddles[m * stride].real(),
+    };
+    xi[12] = {
+        xi[9].imag() * twiddles[m * stride].imag() -
+            xi[10].imag() * twiddles[m * 2 * stride].imag(),
+        xi[10].real() * twiddles[m * 2 * stride].imag() -
+            xi[9].real() * twiddles[m * stride].imag(),
+    };
+    x[m2 + i] = xi[11] + xi[12];
+    x[m3 + i] = xi[11] - xi[12];
+  }
+}
+
+template <typename T>
+static constexpr void ButterflyGeneric(
+    internal::span<T> x, const size_t stride,
+    const std::vector<internal::complex<float>>& twiddles, const size_t m,
+    const size_t p, const size_t N,
+    std::vector<internal::complex<float>>& scratch) {
+  for (size_t i = 0; i < m; ++i) {
+    for (size_t j = 0; j < p; ++j) {
+      scratch[j] = x[i + j * m];
+    }
+
+    for (size_t j = 0; j < p; ++j) {
+      x[i + j * m] = scratch[0];
+      size_t twIdx = 0;
+      for (size_t k = 1; k < p; ++k) {
+        twIdx += stride * (i + j * m);
+        if (twIdx >= N) {
+          twIdx -= N;
+        }
+        x[i + j * m] += scratch[k] * twiddles[twIdx];
+      }
+    }
+  }
+}
+
+template <typename T>
+static constexpr void FftRecursive(
+    const internal::span<const T> x, internal::span<T> y,
+    const size_t inputStride, const size_t factorStride,
+    const size_t recursionIndex, const std::vector<int>& factors,
+    const std::vector<internal::complex<float>>& twiddles, const size_t N,
+    std::vector<internal::complex<float>>& scratch) {
+  const int p = factors[2 * recursionIndex];  // FFT radix for this stage.
   const int m =
-      factors[2 * recursionIndex + 1]; // Length of this FFT stage / radix.
+      factors[2 * recursionIndex + 1];  // Length of this FFT stage / radix.
 
   if (m == 1) {
     // Copy strided input to output.
@@ -209,56 +448,65 @@ FftRecursive(const internal::span<const T> x, internal::span<T> y,
       // each one with a decimated (srided) input.
       FftRecursive(x.subspan(i * factorStride * inputStride), y.subspan(i * m),
                    inputStride, factorStride * p, recursionIndex + 1, factors,
-                   twiddles);
+                   twiddles, N, scratch);
     }
   }
 
   // Recombine the p smaller DFTs.
   switch (p) {
-  case 2:
-    Butterfly2(y, factorStride, twiddles, m);
-    break;
-  case 3:
-    // Butterfly for radix 3.
-    break;
-  case 4:
-    Butterfly4(y, factorStride, twiddles, m);
-    break;
-  case 5:
-    // Butterfly for radix 5.
-    break;
-  default:
-    // Butterfly for generic radix.
-    break;
+    case 2:
+      Butterfly2(y, factorStride, twiddles, m);
+      break;
+    case 3:
+      Butterfly3(y, factorStride, twiddles, m);
+      break;
+    case 4:
+      Butterfly4(y, factorStride, twiddles, m);
+      break;
+    case 5:
+      Butterfly5(y, factorStride, twiddles, m);
+      break;
+    default:
+      ButterflyGeneric(y, factorStride, twiddles, m, p, N, scratch);
+      break;
   }
 }
 
-} // namespace internal
+}  // namespace internal
 
 // Main FFT class.
 class FFT {
-public:
+ public:
   FFT(size_t N, bool inverse) noexcept
-      : N_(N), inverse_(inverse), factors_(internal::Factorize(N_)),
-        twiddles_(internal::ComputeTwiddles<float>(N_, inverse_)) {}
-  FFT(const FFT &) = default;
-  FFT(FFT &&) = default;
+      : N_(N),
+        inverse_(inverse),
+        factors_(internal::Factorize(N_)),
+        twiddles_(internal::ComputeTwiddles<float>(N_, inverse_)),
+        scratch_(internal::RequiredScratchLength(factors_)) {}
+  FFT(const FFT&) = default;
+  FFT(FFT&&) = default;
 
-  void fft(const std::vector<std::complex<float>> &x,
-           std::vector<std::complex<float>> &y) const noexcept {
-    internal::FftRecursive<std::complex<float>>(
-        internal::span<const std::complex<float>>(x.data(), N_),
-        internal::span<std::complex<float>>(y.data(), N_), 1, 1, 0, factors_,
-        twiddles_);
+  void fft(const std::vector<std::complex<float>>& x,
+           std::vector<std::complex<float>>& y) noexcept {
+    // Convert to internal complex type.
+    auto& x_ =
+        reinterpret_cast<const std::vector<kfft::internal::complex<float>>&>(x);
+    auto& y_ =
+        reinterpret_cast<std::vector<kfft::internal::complex<float>>&>(y);
+    internal::FftRecursive<internal::complex<float>>(
+        internal::span<const internal::complex<float>>(x_.data(), N_),
+        internal::span<internal::complex<float>>(y_.data(), N_), 1, 1, 0,
+        factors_, twiddles_, N_, scratch_);
   }
 
-private:
+ private:
   size_t N_;
   bool inverse_;
   std::vector<int> factors_;
-  std::vector<std::complex<float>> twiddles_;
+  std::vector<internal::complex<float>> twiddles_;
+  std::vector<internal::complex<float>> scratch_;
 };
 
-} // namespace kfft
+}  // namespace kfft
 
-#endif // KISS_FFT_PP_H
+#endif  // KISS_FFT_PP_H
--- src/kissFftTest.cpp
+++ src/kissFftTest.cpp
@@ -14,6 +14,121 @@
 
 using namespace kfftTestData;
 
+// Tests for kfft::internal::complex
+TEST(KissFftppInternal, Complex) {
+  kfft::internal::complex<float> c0(1, 2);
+  kfft::internal::complex<float> c1(3, 4);
+
+  // Accessors.
+  EXPECT_EQ(c0.real(), 1);
+  EXPECT_EQ(c0.imag(), 2);
+
+  EXPECT_EQ(c1.real(), 3);
+  EXPECT_EQ(c1.imag(), 4);
+
+  // Modifiers.
+  c0.real(5);
+  c0.imag(6);
+  EXPECT_EQ(c0.real(), 5);
+  EXPECT_EQ(c0.imag(), 6);
+  c0 = {1, 2};
+
+  // Member arithmetic operators.
+  auto c2 = c0 + c1;
+  EXPECT_EQ(c2.real(), 4);
+  EXPECT_EQ(c2.imag(), 6);
+
+  auto c3 = c0 - c1;
+  EXPECT_EQ(c3.real(), -2);
+  EXPECT_EQ(c3.imag(), -2);
+
+  auto c4 = c0 * c1;
+  EXPECT_EQ(c4.real(), -5);
+  EXPECT_EQ(c4.imag(), 10);
+
+  auto c5 = c0 / c1;
+  EXPECT_NEAR(c5.real(), 0.44, 1e-3);
+  EXPECT_NEAR(c5.imag(), 0.08, 1e-3);
+
+  // Non-member arithmetic operators.
+  auto c6 = c0 + 2.0f;
+  EXPECT_EQ(c6.real(), 3);
+  EXPECT_EQ(c6.imag(), 2);
+
+  auto c7 = c0 - 2.0f;
+  EXPECT_EQ(c7.real(), -1);
+  EXPECT_EQ(c7.imag(), 2);
+
+  auto c8 = c0 * 2.0f;
+  EXPECT_EQ(c8.real(), 2);
+  EXPECT_EQ(c8.imag(), 4);
+
+  auto c9 = c0 / 2.0f;
+  EXPECT_EQ(c9.real(), 0.5);
+  EXPECT_EQ(c9.imag(), 1);
+
+  auto c10 = 2.0f + c0;
+  EXPECT_EQ(c10.real(), 3);
+  EXPECT_EQ(c10.imag(), 2);
+
+  auto c11 = 2.0f - c0;
+  EXPECT_EQ(c11.real(), 1);
+  EXPECT_EQ(c11.imag(), -2);
+
+  auto c12 = 2.0f * c0;
+  EXPECT_EQ(c12.real(), 2);
+  EXPECT_EQ(c12.imag(), 4);
+
+  auto c13 = 2.0f / c0;
+  EXPECT_NEAR(c13.real(), 0.4, 1e-7);
+  EXPECT_NEAR(c13.imag(), -0.8, 1e-7);
+
+  auto c14 = -c0;
+  EXPECT_EQ(c14.real(), -1);
+}
+
+// Tests for kfft::internal::span
+TEST(KissFftppInternal, Span) {
+  std::vector<float> v = {1, 2, 3, 4, 5};
+  kfft::internal::span<float> s(v.data(), v.size());
+
+  // Accessors.
+  EXPECT_EQ(s.size(), v.size());
+  EXPECT_EQ(s.data(), v.data());
+  EXPECT_EQ(s[0], 1);
+  EXPECT_EQ(s[1], 2);
+  EXPECT_EQ(s[2], 3);
+  EXPECT_EQ(s[3], 4);
+  EXPECT_EQ(s[4], 5);
+
+  // Modifiers.
+  s[0] = 6;
+  EXPECT_EQ(s[0], 6);
+
+  // Subspan.
+  auto s2 = s.subspan(1, 3);
+  EXPECT_EQ(s2.size(), 3);
+  EXPECT_EQ(s2[0], 2);
+  EXPECT_EQ(s2[1], 3);
+  EXPECT_EQ(s2[2], 4);
+
+// Bounds checking.
+#if !defined(KFFTPP_NO_CONTRACT_CHECKING) && !defined(KFFTPP_NO_EXCEPTIONS)
+  EXPECT_THROW(s[5], std::logic_error);
+  EXPECT_THROW(s.subspan(1, 5), std::logic_error);
+  EXPECT_THROW(s.subspan(5, 1), std::logic_error);
+#endif
+}
+
+TEST(KissFftppInternal, RequiredScratchLength) {
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({2, 1}), 0);
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({3, 1}), 0);
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({4, 1}), 0);
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({5, 1}), 0);
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({6, 1}), 6);
+  EXPECT_EQ(kfft::internal::RequiredScratchLength({6, 1, 13, 2}), 13);
+}
+
 TEST(KissFft, KissFft) {
   auto kfft = kiss_fft_alloc(512, 0, NULL, 0);
   EXPECT_NE(kfft, nullptr);
@@ -67,7 +182,7 @@ TEST(KissFftpp, KissFftpp) {
 }
 
 TEST(KissFftpp, KissFftCompare) {
-  constexpr static size_t N = 16;
+  constexpr static size_t N = 1560;
   std::vector<kiss_fft_cpx> x0(N);
   std::vector<kiss_fft_cpx> y0(N, {0, 0});
 
@@ -98,29 +213,31 @@ TEST(KissFftpp, KissFftCompare) {
   //             << y[i].imag() << "}, ";
   // }
 
-  for (size_t i = 0; i < N; i++) {
-    std::cout << "{ " << std::fixed << std::setprecision(7) << y0[i].r << ", "
-              << y0[i].i << "}, ";
-  }
+  // for (size_t i = 0; i < N; i++) {
+  //   std::cout << "{ " << std::fixed << std::setprecision(7) << y0[i].r << ",
+  //   "
+  //             << y0[i].i << "}, ";
+  // }
 
-  std::cout << std::endl << std::endl;
+  // std::cout << std::endl << std::endl;
+
+  // for (size_t i = 0; i < N; i++) {
+  //   std::cout << "{ " << std::fixed << std::setprecision(7) << y1[i].real()
+  //             << ", " << y1[i].imag() << "}, ";
+  // }
 
   for (size_t i = 0; i < N; i++) {
-    std::cout << "{ " << std::fixed << std::setprecision(7) << y1[i].real()
-              << ", " << y1[i].imag() << "}, ";
-  }
-
-  for (size_t i = 0; i < N; i++) {
-    EXPECT_NEAR(y0[i].r, y1[i].real(), 1e-3);
-    EXPECT_NEAR(y0[i].i, y1[i].imag(), 1e-3);
+    const auto norm = std::sqrt(y0[i].r * y0[i].r + y0[i].i * y0[i].i);
+    EXPECT_NEAR(y0[i].r, y1[i].real(), 1e-3 * norm);
+    EXPECT_NEAR(y0[i].i, y1[i].imag(), 1e-3 * norm);
   }
 }
 
 TEST(KissFftpp, KissFftPerformance) {
   // Run a performance test using many random length 512 ffts.
 
-  constexpr static size_t N = 512;
-  constexpr static size_t NUM_TESTS = 100000;
+  constexpr static size_t N = 1560;
+  constexpr static size_t NUM_TESTS = 10000;
   std::vector<kiss_fft_cpx> x0(N);
   std::vector<kiss_fft_cpx> y0(N, {0, 0});
 
